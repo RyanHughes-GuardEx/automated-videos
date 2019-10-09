6e593af6bb890abf56a1156a01132e1da4c80e92
Ryan Hughes <ryan@guard-ex.net>
2019-10-09 13:42:40 -0400

    vlc control working

--- runvlc.py
+++ runvlc.py
@@ -8,7 +8,11 @@ vlc.clear()
 vlc.loop()         # ToDo: need to ensure this stays toggled off
 vlc.add("videos/eyevideo.avi")
 
-time.sleep(15)
+time.sleep(5)
+vlc.pause()
+time.sleep(2)
+vlc.play()
+time.sleep(7)
 print(vlc.info())
 vlc.stop()
 vlc.disconnect()
--- vlcon.bash
+++ vlcon.bash
@@ -4,5 +4,7 @@
 #/Applications/VLC.app/Contents/MacOS/VLC --intf telnet --telnet-password admin
 
 # with GUI
-/Applications/VLC.app/Contents/MacOS/VLC --extraintf telnet --telnet-password admin
+/Applications/VLC.app/Contents/MacOS/VLC --extraintf telnet --telnet-password admin &
+sleep 1
 python runvlc.py
+osascript -e 'quit app "VLC"'
